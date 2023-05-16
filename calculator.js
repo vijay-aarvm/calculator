47dd0019391b6023beac9b6f7b1c22e3e7fd211b
vijay <vijay.ja@aarvm.com>
2023-05-16 16:58:35 +0530

    added code for keyboard event listening

--- calculator.js
+++ calculator.js
@@ -78,6 +78,88 @@ $(document).ready(function () {
     });
 });
 
+
+$(document).ready(function () {
+    $(".calc-buttons").keydown(function (e) {
+        let out, ex = e.keyCode || e.which;
+        let input = $(this).text();
+        let calculations = $("#user-operations").text();
+        if (e.key == "Delete") {
+            $("#user-operations").text("");
+            $("#result").text("");
+            // reset num1, num2, operator, and total
+            num1 = "";
+            num2 = "";
+            operator = "";
+            total = "";
+        } else if (e.key == "Backspace") {
+            $("#user-operations").text(calculations.slice(0, -1));
+            // remove the last character from num1 or num2
+            if (num2 !== "") {
+                num2 = num2.slice(0, -1);
+            } else if (operator !== "") {
+                operator = "";
+            } else if (num1 !== "") {
+                num1 = num1.slice(0, -1);
+            }
+        } else if (e.key == "Enter") {
+            // evaluate the expression and update total and result
+            handleTotal();
+            if (result !== "" && num2 === "") {
+                $("#user-operations").text(total);
+            } else {
+                $("#result").text(total);
+            }
+        } else if (e.key == ".") {
+            if ((operator === "" && !num1.includes(".")) || (operator !== "" && !num2.includes("."))) {
+                $("#user-operations").append(input);
+                // add the decimal point to num1 or num2
+                if (operator === "") {
+                    num1 += input;
+                } else {
+                    num2 += input;
+                }
+            }
+        } else if (e.key == "%") {
+            // calculate the percentage and update num2
+            total = percentage();
+            $("#user-operations").text(num1 + operator + num2);
+        } else if (e.key == "add" || e.key == "subtract" || e.key == "multiply " || e.key == "divide ") {
+            if (num1 !== "" && num2 === "" && operator === "") {
+                // update the operator if only num1 has been entered
+                operator = input;
+                $("#user-operations").append(input);
+            } else if (num1 === "" && num2 === "" & operator === "") {
+                num1 = 0;
+                if (!isNaN(input)) {
+                    num1 += parseFloat(input);
+                }
+            }  else if (num1 !== "" && num2 === "" && operator !== "") {
+                // replace the previous operator with the new one
+                operator = input;
+                calculations = calculations.slice(0, -1) + input;
+                $("#user-operations").text(calculations);
+            } else if (num1 !== "" && num2 !== "") {
+                // evaluate the expression and update num1, num2, operator, and result
+                handleTotal();
+                num1 = total.toString();
+                num2 = "";
+                operator = input;
+                $("#user-operations").text(num1 + operator);
+                $("#result").text(total);
+            }
+        } else {
+            // handle numeric inputs
+            $("#user-operations").append(input);
+            if (operator === "") {
+                num1 += input;
+            } else {
+                num2 += input;
+            }
+        }
+    });
+});
+
 let num1 = "";
 let num2 = "";
 let operator = "";
